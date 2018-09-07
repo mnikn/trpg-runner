76e66d546d68fc9c40d9249d0935bd58cce569e3
mnikn <329121948@qq.com>
2018-09-07 20:44:29 +0800

    [trival]add unit test

---
+++ tests/reducers/base/navigate-bar.test.ts
@@ -0,0 +1,23 @@
+// import { COC, DND } from '../../../src/base/constants/app-mode';
+
+import { createShowSettingsModalAction, createCloseSettingsModalAction } from './../../../src/base/actions/base/navigate-bar';
+import navigateBar from './../../../src/base/reducers/base/navigate-bar';
+import 'mocha';
+
+import { expect } from 'chai';
+
+describe('Test settings modal', function () {
+    it('should show settings modal', function () {
+        const state = { isSettingsModalVisable: false };
+        const action = createShowSettingsModalAction();
+        const resultState = navigateBar(state, action);
+        expect(resultState.isSettingsModalVisable).to.equal(true);
+    });
+
+    it('should close settings modal', function () {
+        const state = { isSettingsModalVisable: true };
+        const action = createCloseSettingsModalAction();
+        const resultState = navigateBar(state, action);
+        expect(resultState.isSettingsModalVisable).to.equal(false);
+    });
+})
