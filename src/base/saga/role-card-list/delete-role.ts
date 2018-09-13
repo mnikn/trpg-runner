2ba0f7ec4b31b07d4d964b57fac09df5cdcd0471
mnikn <329121948@qq.com>
2018-09-13 22:22:54 +0800

    [bug]fix delete role select issue

--- src/base/saga/role-card-list/delete-role.ts
+++ src/base/saga/role-card-list/delete-role.ts
@@ -1,9 +1,15 @@
+import { appStore } from './../../../../index';
+import { createUpdateButtonOnSelectingAction } from './../../actions/base/app';
 import { ACTION_DND_DELETE_ROLE_SUCCESS } from './../../actions/dnd/dnd';
 import { call, put } from 'redux-saga/effects';
 import RoleDataService from "../../../dnd/services/role-data-service";
+import * as _ from 'lodash';
 
 export function* deleteDndRole(action: any) {
     let promise = RoleDataService.deleteRole(action.deleteRoleIds);
     const data = yield call(() => promise);
     yield put({ type: ACTION_DND_DELETE_ROLE_SUCCESS, data });
+
+    let selectedRoles = appStore.getState().roleCardList.selectedRoles.filter((id: number) => !_.includes(data, id));
+    yield put(createUpdateButtonOnSelectingAction(selectedRoles));
 }
